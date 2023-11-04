941dcbb63bb0382df74ce7e490eb80c5b3dd6ae2
Kosma Gasiorowski <s188660@student.pg.edu.pl>
2023-11-04 23:42:35 +0000

    updated build.sh script for compatibility

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ cd ..
 python3 setup.py bdist_wheel
 
 echo -e "${RED}Installing Python wheel and upgrading packages...${NC}"
-cd ../python-test/
-./.venv/bin/pip install -U ../spg-opengl/dist/display-0.0.1-cp310-cp310-linux_aarch64.whl --force-reinstall
+./venv/bin/pip install -U ../spg-opengl/dist/display-0.0.1-cp310-cp310-linux_aarch64.whl --force-reinstall
 
 echo -e "${RED}Finished running the script.${NC}"
